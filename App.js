ed9af51e196603804729c6691142fe390b7f0d98
Nico96C <nicolas.cuello96@hotmail.com>
2024-07-15 13:18:45 -0300

    guardado de cambios

--- App.js
+++ App.js
@@ -1,12 +1,20 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, Image, Button } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
       <StatusBar style="Light" />
-      <Image source={{ uri: "https://www.metacritic.com/a/img/resize/51057e1e2b2e488b9a645fe7ad6aa90a3039f433/catalog/provider/6/3/6-1-899576-13.jpg?auto=webp&fit=cover&height=300&width=200"}} style={{width: 242, height: 300}} />
-      <Text style={styles.texto}>Open up App.js to start working on your app! este texto en español!</Text>
+      <Image
+        source={{
+          uri: "https://www.metacritic.com/a/img/resize/51057e1e2b2e488b9a645fe7ad6aa90a3039f433/catalog/provider/6/3/6-1-899576-13.jpg?auto=webp&fit=cover&height=300&width=200",
+        }}
+        style={{ width: 242, height: 300 }}
+      />
+      <Text style={styles.texto}>
+        Open up App.js to start working on your app! este texto en español!
+      </Text>
+      <Button title="Pulsa aqui" onPress={() => alert("Hola")} />
     </View>
   );
 }
@@ -14,11 +22,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#212121',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#212121",
+    alignItems: "center",
+    justifyContent: "center",
   },
   texto: {
-    color: '#fff',
-  }
+    color: "#fff",
+  },
 });
