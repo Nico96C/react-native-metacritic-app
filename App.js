42f49a6c58c2c4152968c0316c4868c2b98bbb4d
Nico96C <nicolas.cuello96@hotmail.com>
2024-07-15 11:30:54 -0300

    comienzo react native

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <StatusBar style="Light" />
+      <Image source={{ uri: "https://www.metacritic.com/a/img/resize/51057e1e2b2e488b9a645fe7ad6aa90a3039f433/catalog/provider/6/3/6-1-899576-13.jpg?auto=webp&fit=cover&height=300&width=200"}} style={{width: 242, height: 300}} />
+      <Text style={styles.texto}>Open up App.js to start working on your app! este texto en español!</Text>
     </View>
   );
 }
@@ -13,8 +14,11 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#212121',
     alignItems: 'center',
     justifyContent: 'center',
   },
+  texto: {
+    color: '#fff',
+  }
 });
