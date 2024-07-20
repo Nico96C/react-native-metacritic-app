b0d0a9a6c9012153f4cd808a9b47e5a230ecde63
Nico96C <nicolas.cuello96@hotmail.com>
2024-07-20 14:27:58 -0300

    Agregadas dependencias de expo

--- App.js
+++ App.js
@@ -1,21 +1,17 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, Image, Button } from "react-native";
+
+import { StyleSheet, View } from "react-native";
+import { Main } from "./components/Main";
+import { SafeAreaProvider } from "react-native-safe-area-context";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StatusBar style="Light" />
-      <Image
-        source={{
-          uri: "https://www.metacritic.com/a/img/resize/51057e1e2b2e488b9a645fe7ad6aa90a3039f433/catalog/provider/6/3/6-1-899576-13.jpg?auto=webp&fit=cover&height=300&width=200",
-        }}
-        style={{ width: 242, height: 300 }}
-      />
-      <Text style={styles.texto}>
-        Open up App.js to start working on your app! este texto en español!
-      </Text>
-      <Button title="Pulsa aqui" onPress={() => alert("Hola")} />
-    </View>
+    <SafeAreaProvider>
+      <View style={styles.container}>
+        <StatusBar style="Light" />
+        <Main />
+      </View>
+    </SafeAreaProvider>
   );
 }
 
@@ -25,8 +21,5 @@ const styles = StyleSheet.create({
     backgroundColor: "#212121",
     alignItems: "center",
     justifyContent: "center",
-  },
-  texto: {
-    color: "#fff",
   },
 });
---
+++ components/Main.jsx
@@ -0,0 +1,56 @@
+import { useEffect, useState } from "react";
+
+import { StyleSheet, View, Image, Text, ScrollView } from "react-native";
+import { getLatestGames } from "../lib/metacritic";
+
+export function Main() {
+  const [games, setGames] = useState([]);
+
+  useEffect(() => {
+    getLatestGames().then((games) => {
+      setGames(games);
+    });
+  }, []);
+
+  return (
+    <>
+      <ScrollView>
+        {games.map((game) => (
+          <View key={game.slug} style={styles.card}>
+            <Image source={{ uri: game.image }} style={styles.image} />
+            <Text style={styles.title}> {game.title} </Text>
+            <Text style={styles.score}> {game.score} </Text>
+            <Text style={styles.description}> {game.description} </Text>
+          </View>
+        ))}
+      </ScrollView>
+    </>
+  );
+}
+
+const styles = StyleSheet.create({
+  card: {
+    marginBottom: 42,
+  },
+  image: {
+    width: 107,
+    height: 147,
+    borderRadius: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: "bold",
+    marginTop: 10,
+    color: "#fff",
+  },
+  description: {
+    fontSize: 16,
+    color: "#eee",
+  },
+  score: {
+    fontSize: 20,
+    fontWeight: "bold",
+    color: "green",
+    marginBottom: 10,
+  },
+});
